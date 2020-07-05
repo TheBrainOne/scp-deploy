59ad90b92bf598fdb222bb1a233b0d2c2da7ab83
Алексей Долженко <thebrainone@MacBook-Pro-Aleksej.local>
2020-07-05 22:16:12 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/sh -l
 
-echo "hello $1"
+# Import SSH key from Github secret then create a temporary key file
+echo -e "${INPUT_SSH_KEY}" >./gh-action/.github/workflows/_temp_ssh_key
+
+# Change file access permissions.
+chmod 600 ./gh-action/.github/workflows/_temp_ssh_key
+
+# Trying to deploy files from the repository to the server
+# scp
+# -o <ssh_option> : StrictHostKeyChecking - 
+# -v : Display the operation’s execution step by step.
+# -i <identity_file> : Sets file name of the private SSH key file.
+# -P <port> : Connect to port on the remote host.
+# -r : Copy recursively.
+# Local path to files/folders
+# username@host:path on the server where you want to deploy
+scp -o StrictHostKeyChecking=no -v -i ./gh-action/.github/workflows/_temp_ssh_key -P "${INPUT_PORT}" -r ${INPUT_LOCAL_PATH} "${INPUT_USERNAME}"@"${INPUT_HOST}":"${INPUT_SERVER_PATH}"

bf1d0921ef66b3a643e0e0bd89c7454f585bdb5a
Алексей Долженко <thebrainone@MacBook-Pro-Aleksej.local>
2020-07-05 22:21:24 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
 # Import SSH key from Github secret then create a temporary key file
-echo -e "${INPUT_SSH_KEY}" >./gh-action/.github/workflows/_temp_ssh_key
+echo -e "${INPUT_SSH_KEY}" >./.github/workflows/_temp_ssh_key
 
 # Change file access permissions.
-chmod 600 ./gh-action/.github/workflows/_temp_ssh_key
+chmod 600 ./.github/workflows/_temp_ssh_key
 
 # Trying to deploy files from the repository to the server
 # scp
@@ -15,4 +15,4 @@ chmod 600 ./gh-action/.github/workflows/_temp_ssh_key
 # -r : Copy recursively.
 # Local path to files/folders
 # username@host:path on the server where you want to deploy
-scp -o StrictHostKeyChecking=no -v -i ./gh-action/.github/workflows/_temp_ssh_key -P "${INPUT_PORT}" -r ${INPUT_LOCAL_PATH} "${INPUT_USERNAME}"@"${INPUT_HOST}":"${INPUT_SERVER_PATH}"
+scp -o StrictHostKeyChecking=no -v -i ./.github/workflows/_temp_ssh_key -P "${INPUT_PORT}" -r ${INPUT_LOCAL_PATH} "${INPUT_USERNAME}"@"${INPUT_HOST}":"${INPUT_SERVER_PATH}"

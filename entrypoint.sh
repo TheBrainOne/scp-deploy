e0417413d4d578166a0bdd5f1b4f4880ebc91ee3
Alexey Dolzhenko <tooroff@gmail.com>
2020-07-08 01:58:27 +0700

    feat: Проверка на установку пакетов на сервере

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,3 +16,7 @@ chmod 600 ./.github/workflows/_temp_ssh_key
 # Local path to files/folders
 # username@host:path on the server where you want to deploy
 scp -o StrictHostKeyChecking=no -v -i ./.github/workflows/_temp_ssh_key -P "${INPUT_PORT}" -r ${INPUT_LOCAL_PATH} "${INPUT_USERNAME}"@"${INPUT_HOST}":"${INPUT_SERVER_PATH}"
+
+if ${INPUT_INIT_NPM} 
+then ssh -p "${INPUT_PORT}" -i ./.github/workflows/_temp_ssh_key "${INPUT_USERNAME}"@"${INPUT_HOST}" "${INPUT_SERVER_PATH} && npm i"
+fi
